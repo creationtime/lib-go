11ac54583961dac7eb4cc84f7fc6052099b429a2
zhaojinhui <494490727@qq.com>
2021-05-19 11:18:42 +0800

    log更新

--- logger/zerolog/zerolog_test.go
+++ logger/zerolog/zerolog_test.go
@@ -2,8 +2,17 @@ package zerolog
 
 import (
 	"testing"
+
+	"github.com/rs/zerolog"
 )
 
 func TestName(t *testing.T) {
-	Log().Infof("username:%s, userId:%d", "test", 1)
+	Logger.Info().Msg("hello word")
+	Logger.Info().Int("userId", 100).Msg("create user")
+	Logger.Info().
+		Dict("userInfo", zerolog.Dict().Str("username", "tom").Int("age", 10)).
+		Float64("money", 11.11).
+		Bool("exist", true).
+		Msg("hello")
+	Logger.Info().Caller(0).Send()
 }
